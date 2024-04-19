a555367fa003b61777bfb097f4c1541d75da2cb1
Darwin Burbano <carma-ddbg@hotmail.com>
2024-04-19 09:33:39 -0500

    modificacion

---
+++ config/database.php
@@ -0,0 +1,17 @@
+<?php
+    //DataBase configuration
+    $host = "127.0.0.1"; // =>localhost
+    $dbname = "Pets";
+    $username = "postgres";
+    $password = "123456789";
+    $port = "5432";
+
+    
+    $conn = pg_connect("
+    host=$host
+    dbname=$dbname
+    user=$username
+    password=$password
+    port=$port
+    ");
+?>
