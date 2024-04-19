112a547fd6380c7fa02acf222fdb02e4c06a760f
Darwin Burbano <carma-ddbg@hotmail.com>
2024-04-19 09:34:22 -0500

    modificacion

--- src/backend/signup.php
+++ src/backend/signup.php
@@ -1,10 +1,14 @@
 <?php
     $fullname = $_POST ["fname"];
+    $telefono = $_POST ["telefono"];
+    $date = $_POST ["fecha"];
     $email = $_POST ["email"];
     $password = $_POST ["password"];
     $enc_pass = md5($password);
 
     echo "Your fullname :". $fullname. "<br>";
+    echo "Your telefono :". $telefono. "<br>";
+    echo "Your fecha :". $date. "<br>";
     echo "Your email :". $email. "<br>";
     echo "Your password :". $enc_pass;
 
