a302a9da82991d86d3921f8d5fbd800efca45e9f
Darwin Burbano <carma-ddbg@hotmail.com>
2024-04-19 11:09:26 -0500

    actualizacion 19/04/2024

--- src/backend/signup.php
+++ src/backend/signup.php
@@ -1,4 +1,6 @@
 <?php
+include("../../config/database.php");
+
     $fullname = $_POST ["fname"];
     $telefono = $_POST ["telefono"];
     $date = $_POST ["fecha"];
@@ -6,10 +8,21 @@
     $password = $_POST ["password"];
     $enc_pass = md5($password);
 
-    echo "Your fullname :". $fullname. "<br>";
-    echo "Your telefono :". $telefono. "<br>";
-    echo "Your fecha :". $date. "<br>";
-    echo "Your email :". $email. "<br>";
-    echo "Your password :". $enc_pass;
+
+    $sql = "
+        INSERT INTO users (fullname, telefono, fecha, email, password) 
+            VALUES ('$fullname','$telefono','$date', '$email','$enc_pass')
+    ";
+
+    $ans = pg_query($conn,$sql);    
+        if ($ans){
+        echo "User has been created successfully";
+        }else{
+        echo "Error: " . pg_last_error();
+        }
+
+    //Close connection
+    pg_close($conn)
+
 
 ?>
