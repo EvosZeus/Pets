284792fe332ba6b6bffad8400f48509324ed07d7
Darwin Burbano <carma-ddbg@hotmail.com>
2024-04-29 18:01:48 -0500

    signin.php

---
+++ src/backend/signin.php
@@ -0,0 +1,32 @@
+<?php
+    include("../../config/database.php");
+
+    $email = $_POST ["email"];
+    $password = $_POST ["password"];
+    $enc_pass = md5($password);
+   
+    
+    $sql = "
+        SELECT 
+        * 
+        FROM 
+            users 
+        WHERE 
+        email ='$email' AND 
+        password ='$enc_pass'
+        Limit 1
+        ";
+
+$result = pg_query($conn,$sql);
+$total = pg_num_rows($result);
+if ($total > 0 ){
+    echo "login OK";
+
+}else {
+    echo "Credenciales incorrectas"
+}
+
+
+
+
+?>
